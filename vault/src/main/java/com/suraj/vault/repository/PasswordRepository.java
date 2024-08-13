15d3b97d16cd8ddc2547cfa001ffa5d8db56f8a6
suraj7wce <lokapuresa7@gmail.com>
2024-08-13 13:52:23 +0530

    Delete vault directory

--- vault/src/main/java/com/suraj/vault/repository/PasswordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.suraj.vault.repository;
-
-import com.suraj.vault.entity.PasswordEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface PasswordRepository extends JpaRepository<PasswordEntry, Long> {
-    Optional<PasswordEntry> findByWebsiteOrAppName(String websiteOrAppName);
-}
--- vault/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS passwords (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    website_or_app_name VARCHAR(255) NOT NULL,
-    encrypted_password TEXT NOT NULL
-);
